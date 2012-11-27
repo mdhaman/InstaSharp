8aee81bc2ef5301025c0bb58e0e11a1e2eb396d7
Mayur Dhamanwala <dhamanwalam@aap.com.au>
2012-11-27 17:56:18 +1100

    added caption class to the mediaresponse class.

---
+++ Model/Caption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InstaSharp.Model
+{
+    public class Caption
+    {
+        [JsonMapping("created_time", JsonMapping.MappingType.Primitive)]
+        public DateTime CreatedTime { get; set; }
+
+        [JsonMapping("text", JsonMapping.MappingType.Primitive)]
+        public string Text { get; set; }
+
+        [JsonMapping("from", JsonMapping.MappingType.Class)]
+        public User From { get; set; }
+
+        [JsonMapping("id", JsonMapping.MappingType.Primitive)]
+        public string Id { get; set; }
+    }
+}
